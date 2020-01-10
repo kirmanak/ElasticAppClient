918382f2bace4bbb7d772b5ba8458a0423662097
Kirill Kamakin <k.kamakin@protonmail.ch>
2020-01-10 23:31:19 +0300

    Multiply OpenNebula memory usage by 1024 to convert to bytes
    Signed-off-by: Kirill Kamakin <k.kamakin@protonmail.ch>

--- src/main/kotlin/ru/ifmo/kirmanak/infrastructureclient/opennebula/OpenNebulaNode.kt
+++ src/main/kotlin/ru/ifmo/kirmanak/infrastructureclient/opennebula/OpenNebulaNode.kt
@@ -7,7 +7,7 @@ import ru.ifmo.kirmanak.infrastructureclient.AppNode
 internal class OpenNebulaNode(vm: VirtualMachine) : AppNode {
     private val name: String = vm.name ?: throw AppClientException("Unknown vm name")
     private val cpuLoad = getUsage("CPU", vm)
-    private val memoryLoad = getUsage("MEMORY", vm)
+    private val memoryLoad = getUsage("MEMORY", vm) * 1024
 
     override fun getCPULoad() = cpuLoad
 
