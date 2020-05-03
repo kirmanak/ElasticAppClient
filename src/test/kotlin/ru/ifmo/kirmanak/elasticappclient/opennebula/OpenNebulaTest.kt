d7560e04a50e77aa63e27f502fa6cbeebeebc53c
Kirill Kamakin <k.kamakin@protonmail.ch>
2020-05-03 22:03:27 +0300

    Read OpenNebula test VM group, role and template from environment
    Signed-off-by: Kirill Kamakin <k.kamakin@protonmail.ch>

--- src/test/kotlin/ru/ifmo/kirmanak/elasticappclient/opennebula/OpenNebulaTest.kt
+++ src/test/kotlin/ru/ifmo/kirmanak/elasticappclient/opennebula/OpenNebulaTest.kt
@@ -3,26 +3,28 @@ package ru.ifmo.kirmanak.elasticappclient.opennebula
 import org.junit.Test
 import org.opennebula.client.Client
 import ru.ifmo.kirmanak.elasticappclient.AppClient
-import ru.ifmo.kirmanak.elasticappclient.AppClientFactory
+import ru.ifmo.kirmanak.elasticappclient.AppClientFactory.Companion.getClient
 
 private const val ENV_LOGIN = "OPEN_NEBULA_LOGIN_TEST"
 private const val ENV_PASS = "OPEN_NEBULA_PASSWORD_TEST"
 private const val ENV_ADDR = "OPEN_NEBULA_ADDRESS_TEST"
+private const val ENV_GROUP = "OPEN_NEBULA_GROUP_TEST"
+private const val ENV_ROLE = "OPEN_NEBULA_ROLE_TEST"
+private const val ENV_TEMPLATE = "OPEN_NEBULA_TEMPLATE_TEST"
 
 class OpenNebulaTest {
     private val oneClient: AppClient
 
     private companion object {
-        fun getEnv(name: String) =
+        fun getStringEnv(name: String) =
             System.getenv(name) ?: throw IllegalArgumentException("Set $name environmental variable")
+
+        fun getIntEnv(name: String) = getStringEnv(name).toInt()
     }
 
     init {
-        val login = getEnv(ENV_LOGIN)
-        val password = getEnv(ENV_PASS)
-        val address = getEnv(ENV_ADDR)
-        val client = Client("${login}:${password}", address)
-        oneClient = AppClientFactory.getClient(client, 0, 0, 1)
+        val client = Client("${getStringEnv(ENV_LOGIN)}:${getStringEnv(ENV_PASS)}", getStringEnv(ENV_ADDR))
+        oneClient = getClient(client, getIntEnv(ENV_GROUP), getIntEnv(ENV_ROLE), getIntEnv(ENV_TEMPLATE))
     }
 
     @Test
